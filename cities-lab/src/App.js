8c2edc4f3b7765367f7438c42c9c7df544e8e269
SullyDurgin <katsdurgin@gmail.com>
2021-12-03 17:27:21 -0500

    final

--- cities-lab/src/App.js
+++ cities-lab/src/App.js
@@ -1,30 +1,40 @@
 // IMPORT useState
-import React from "react";
-import "./styles.css";
+import React, { useState } from 'react'
+import './styles.css'
+import imagesArr from './imageData'
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage]
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
- const [bigImage, setBigImage] = useState({
-   img: imageArr[0].img,
-   city: imagesArr[0].city
- })
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+	// USE useState TO CREATE  [bigImage, setBigImage]
+	// AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+	const [bigImage, setBigImage] = useState(imagesArr[0].img)
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
-  // ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
+	// CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+	// THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+	const selectImage = (imageUrl) => {
+		setBigImage(imageUrl);
+	};
 
-  return (
-    <div className="App">
-      <h1>Cities Of The World</h1>
-      <div id="wrapper">
-        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        <img src={bigImage} id="bigimage" alt="bigImage"/>
-      </div>
-    </div>
-  );
+	// CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+	const images = imagesArr.map((ele, index) => {
+		return (
+			<img src={ele.img} alt={ele.city} className='thumb' key={index}
+      onClick={() => selectImage(ele.img)}
+      />
+		);
+	});
+
+	// ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
+	// ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
+
+	return (
+		<div className='App'>
+			<h1>Cities Of The World</h1>
+			<div id='wrapper'>
+				<div id='thumbnails'>{images}</div>
+				{/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+				<img src={bigImage} id='bigimage' alt='bigImage' />
+			</div>
+		</div>
+	)
 }
